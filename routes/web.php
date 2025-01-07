998d568790d36f2ac779390e9c98f30e78135a40
Amorais19 <alicemoraisams@gmail.com>
2025-01-07 12:29:31 -0300

    ii

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::post('/calcular', [App\Http\Controllers\ControllerCalculo::class, 'Calcular'])->name('calcular');
+
